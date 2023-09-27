f1b0248a60a975f20f952707b123217379c9172f
Raquel Ortiz Lopez <rortiz2401@gmail.com>
2023-09-27 10:02:04 -0600

    HW M1-01

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -1,8 +1,22 @@
 'use strict';
 
-function BinarioADecimal(num) {}
+function BinarioADecimal(num) {
+   num = num.split('').reverse();
+   let suma = 0;
+   for (let i = 0; i < num.length; i++){
+      suma += Math.pow(2, i) * num[i];
+   }
+   return suma;
+}
 
-function DecimalABinario(num) {}
+function DecimalABinario(num) {
+   let binario = [];
+   while (num !== 0){
+      binario.unshift(num % 2);
+      num = Math.floor(num / 2);
+   }
+   return binario.join('');
+}
 
 module.exports = {
    BinarioADecimal,
