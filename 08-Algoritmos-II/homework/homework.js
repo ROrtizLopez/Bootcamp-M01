db2f3034542907414aba91a21afe866335b04bb2
Raquel Ortiz Lopez <rortiz2401@gmail.com>
2023-10-04 19:00:12 -0600

    HW08 - M01

--- 08-Algoritmos-II/homework/homework.js
+++ 08-Algoritmos-II/homework/homework.js
@@ -7,7 +7,19 @@ function quickSort(array) {
   // Devolver el array ordenado resultante
   // Tu código:
 
-}
+  if(array.length <= 1) return array; // caso de corte.
+
+  let pivot = array[0];
+  let menores = [];
+  let mayores = [];
+
+  for (let i = 1; i < array.length; i++){
+      if(array[i] < pivot) menores.push(array[i]);
+      else mayores.push(array[i]);
+    }; 
+
+  return [...quickSort(menores), pivot,...quickSort(mayores)];
+};
 
 function mergeSort(array) {
   // Implementar el método conocido como mergeSort para ordenar de menor a mayor
@@ -15,8 +27,26 @@ function mergeSort(array) {
   // Devolver el array ordenado resultante
   // Tu código:
 
-  //okey probemos ahora
-}
+  if(array.length <= 1) return array; // caso de corte.
+
+  let half = Math.floor(array.length / 2);
+
+  let firstHalf = array.slice(0, half);
+  let lastHalf = array.slice(half); 
+
+  firstHalf = mergeSort(firstHalf); 
+  lastHalf = mergeSort(lastHalf);
+
+  array = [];
+
+  while(firstHalf.length && lastHalf.length){ 
+    if (firstHalf[0] < lastHalf[0]) array.push(firstHalf.shift());
+    else array.push(lastHalf.shift());
+};
+
+return array.concat(firstHalf, lastHalf);
+
+};
 
 // No modificar nada debajo de esta línea
 // --------------------------------
